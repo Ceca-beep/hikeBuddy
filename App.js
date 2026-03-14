c74c16030cd64228738cd04976dff41e201a1942
biancatanul <bianca.tanul05@e-uvt.ro>
2026-03-14 16:59:28 +0200

    added trail details screen and additional signup fields

--- App.js
+++ App.js
@@ -5,17 +5,20 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from './screens/Login';
 import SignUp from './screens/SignUp';
 import Homescreen from './screens/Homescreen';
+import TrailDetails from './screens/TrailDetails';
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-          <Stack.Screen name="Home" component={Homescreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
+                <Stack.Screen name="Login" component={Login}/>
+                <Stack.Screen name="SignUp" component={SignUp}/>
+                <Stack.Screen name="Home" component={Homescreen}/>
+                <Stack.Screen name="TrailDetail" component={TrailDetails}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
