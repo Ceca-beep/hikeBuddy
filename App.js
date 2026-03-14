e751ff812c510c16164c2511b5f9f42e457794bb
predutalexia <alexia.predut05@e-uvt.ro>
2026-03-14 21:13:03 +0200

    frontend bugs fixed

--- App.js
+++ App.js
@@ -5,17 +5,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from './screens/Login';
 import SignUp from './screens/SignUp';
 import Homescreen from './screens/Homescreen';
+import TrailDetails from './screens/TrailDetails';
+import Mapscreen from './screens/Mapscreen'
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-          <Stack.Screen name="Home" component={Homescreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
+                <Stack.Screen name="Login" component={Login}/>
+                <Stack.Screen name="SignUp" component={SignUp}/>
+                <Stack.Screen name="Home" component={Homescreen}/>
+                <Stack.Screen name="TrailDetail" component={TrailDetails}/>
+                <Stack.Screen name="Mapscreen" component={Mapscreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
